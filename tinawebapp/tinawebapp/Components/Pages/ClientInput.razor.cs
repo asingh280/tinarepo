7e27886fdc11becf5050df7483c8959933d68a14
Arshnoor Singh <arshnoorsingh916@gmail.com>
2024-01-16 21:29:33 -0700

    haaaaarrrrrrsssshhhpppprrreeeetttt

---
+++ tinawebapp/tinawebapp/Components/Pages/ClientInput.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+
+namespace tinawebapp.Components.Pages
+{
+    public partial class ClientInput
+    {
+        private TaskModel task = new TaskModel { StartDate = DateTime.Today };
+
+        public class TaskModel
+        {
+            public string EstimatedTime { get; set; }
+            public DateTime StartDate { get; set; } = DateTime.Today;
+            public DateTime DueDate { get; set; }
+            public string Type { get; set; }
+            public string Priority { get; set; }
+            public string Description { get; set; }
+        }
+    }
+}
+
+
