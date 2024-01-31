f32adbb09ba706576ea6202c8b2f83b4e046255f
Arshnoor Singh <arshnoorsingh916@gmail.com>
2024-01-30 19:08:44 -0700

    copy

--- tinawebapp/tinawebapp/Data/ApplicationDbContext.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-
-namespace tinawebapp.Data
-{
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
-    {
-    }
-}
